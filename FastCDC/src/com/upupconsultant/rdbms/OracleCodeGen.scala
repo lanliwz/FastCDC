b52d4d083072cc213e149b759e6177ce4ddac264
lanliwz <lanliwz@yahoo.com>
2015-02-24 20:46:11 -0500

    add slick test

--- FastCDC/src/com/upupconsultant/rdbms/OracleCodeGen.scala
+++ FastCDC/src/com/upupconsultant/rdbms/OracleCodeGen.scala
@@ -36,5 +36,30 @@ object OracleCodeGen {
       case "COF_INVENTORY" => "CoffeeInventoryItem"
       case _               => super.entityName(dbTableName)
     }
+        override def tableName = dbTableName => dbTableName match {
+      case "COF_INVENTORY" => "CoffeeInventory"
+      case _ => super.tableName(dbTableName)
+    }
+    // override generator responsible for tables
+    override def Table = new Table(_){
+      table =>
+      // customize table value (TableQuery) name (uses tableName as a basis)
+      override def TableValue = new TableValue{
+        override def rawName = super.rawName.uncapitalize
+      }
+      // override generator responsible for columns
+      override def Column = new Column(_){
+        // customize Scala column names
+        override def rawName = (table.model.name.table,this.model.name) match {
+          case ("COFFEES","COF_NAME") => "name"
+          case ("COFFEES","SUP_ID") => "supplierId"
+          case ("SUPPLIERS","SUP_ID") => "id"
+          case ("SUPPLIERS","SUP_NAME") => "name"
+          case ("COF_INVENTORY","QUAN") => "quantity"
+          case ("COF_INVENTORY","COF_NAME") => "coffeeName"
+          case _ => super.rawName
+        }
+      }
+    }
   }
 }
--- FastCDC/src/com/upupconsultant/rdbms/OracleSlickConnector.scala
+++ FastCDC/src/com/upupconsultant/rdbms/OracleSlickConnector.scala
@@ -5,6 +5,8 @@ import scala.util.Properties._
 object OracleSlickConnector {
   val slickdriver = "com.typesafe.slick.driver.oracle.OracleDriver"
   val jdbcdriver = "oracle.jdbc.driver.OracleDriver"
-  val db = Database.forURL("jdbc:oracle:thin:@localhost:1521:DIODS5", "wzhang", "welcome123", null, "oracle.jdbc.driver.OracleDriver")
-
+  val db = Database.forURL(url="jdbc:oracle:thin:@localhost:1521:DIODS6", user="wzhang", password="welcome123" , driver="oracle.jdbc.driver.OracleDriver")
+  def database(dbname:String,user:String) = dbname match {
+    case "DIODS6" => Database.forURL(url="jdbc:oracle:thin:@localhost:1521:DIODS6", user="wzhang", password="welcome123" , driver="oracle.jdbc.driver.OracleDriver")
+  }
 }
